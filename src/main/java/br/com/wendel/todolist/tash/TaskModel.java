3f1d7e73c9c604539699c56c1b5d6e8eeb7cc4e9
WendelSantosNunes <wendelnunes9999@gmail.com>
2023-10-13 09:25:06 -0300

    feat: Return a more user-friendly error message when I had a task with more than 50 characters

--- src/main/java/br/com/wendel/todolist/tash/TaskModel.java
+++ src/main/java/br/com/wendel/todolist/tash/TaskModel.java
@@ -17,8 +17,8 @@ public class TaskModel {
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
-
   private String description;
+
   @Column(length = 50)
   private String title;
   private LocalDateTime startAt;
@@ -28,5 +28,14 @@ public class TaskModel {
   private UUID idUser;
 
   @CreationTimestamp
-  private LocalDateTime createdAt;   
+  private LocalDateTime createdAt;
+  
+  public void setTitle(String title) throws Exception{
+    
+    if(title.length() > 50){
+      throw new Exception("O campo title deve conter no máximo 50 caracteres");
+    }
+
+    this.title = title;
+  }
 }
